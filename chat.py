72a734f4f34484d783039792fcc69d596393812e
Bilal Zaidi <40125492+bilalzaidi88@users.noreply.github.com>
2019-11-17 16:44:53 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 from app import create_app, socketio
 
-app = create_app(debug=True)
+# app = create_app(debug=True)
+app = Flask(__name__)
+socketio = SocketIO(app)
 
 if __name__ == '__main__':
     socketio.run(app)
